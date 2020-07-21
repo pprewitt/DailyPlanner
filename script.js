76ed7197a11bc50e6173d22f438291e7df37647e
Dr Phil <philip.prewitt@gmail.com>
2020-07-21 15:50:36 -0600

    styling icons padding with js

--- script.js
+++ script.js
@@ -10,21 +10,56 @@ $('#currentDay').text(dayDateTime.format('[Today is] dddd, MMMM, Do, YYYY'));
 //get localstoragetodos
     //parse JSON
 //create planner divs 12 add class of row and a styling class, add attribute for indexing
-
-for(let hours=0; hours<12; hours++){
+var planContainer = $('.container');
+for(var hours=7; hours<20; hours++){
     var planHourDiv = $('<div>');
     planHourDiv.addClass('row plan-row');
     planHourDiv.attr('hours-index', hours);
-    $(".container").append(planHourDiv)
+    planContainer.append(planHourDiv)
 
 //create divs for and style classes for time, todo, save: col-md-3,8,1 respectively
     var colDivTime = $('<div>');
-    colDivTime.addClass('col-md-3 time-column border-top border-bottom');
+    colDivTime.addClass('col-md-3 time-column border-top border-bottom p-3');
+    
+
+
+    
+    var timeSpan = $('<span>');
+    //cant get hours to populate properly
+    var hour = $(planHourDiv['hours-index']);
+    
+    
+    var hourDisplay = 0;
+    var amPm = "";
+    if (hour>12) {
+        hourDisplay = hour-12;
+        amPm = "am";
+    }else {
+            displayHour = hour;
+            ampm = "am";
+          };
+    timeSpan.text(hourDisplay, amPm);
+    
+    $(planHourDiv).append(colDivTime);
+    
+    $(colDivTime).append(timeSpan);
+
     var colDivEntry = $('<div>');
-    colDivEntry.addClass('col-md-8 entry-column bg-danger');
+    colDivEntry.addClass('col-md-8 entry-column bg-danger p-3');
+    var entryInput = $('<input>');
+    entryInput.addClass('form-control');
+    colDivEntry.append(entryInput);
+    planHourDiv.append(colDivEntry);
+
+
     var colDivSave = $('<div>');
-    colDivSave.addClass('col-md-1 save-column bg-secondary');
-    $(planHourDiv).append(colDivTime, colDivEntry, colDivSave);
+    colDivSave.addClass('col-md-1 save-column bg-secondary p-3');
+    var saveBtnIcon = $('<i>');
+    saveBtnIcon.addClass('far fa-save');      
+    colDivSave.append(saveBtnIcon);
+    planHourDiv.append(colDivSave);
+
+    // , colDivEntry, colDivSave
     
 }
 
